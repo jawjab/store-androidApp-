deba5a7e9c5d1ea3e76f14867f945a1820108afd
Jab <Jab@Jabs-MacBook-Air.local>
2016-06-01 13:13:58 +0700

    3rd commit and store page cart page finish

--- app/src/main/java/com/joj/app/Main.java
+++ app/src/main/java/com/joj/app/Main.java
@@ -1,13 +1,58 @@
 package com.joj.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
+    private EditText user;
+    private EditText pass;
+    private Button login;
+    private TextView signup;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponent();
+        actionListener();
     }
+
+    public void initComponent() {
+        this.user = (EditText) findViewById(R.id.textUser);
+        this.pass = (EditText) findViewById(R.id.textPass);
+        this.login = (Button) findViewById(R.id.btnLogin);
+        this.signup = (TextView) findViewById(R.id.texSignup);
+    }
+
+    public void actionListener() {
+        this.login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        intent = new Intent(Main.this, Store.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+        );
+        this.signup.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        intent = new Intent(Main.this, SignUp.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+        );
+
+    }
+
+
 }
--- app/src/main/java/com/joj/app/SignUp.java
+++ app/src/main/java/com/joj/app/SignUp.java
@@ -1,13 +1,81 @@
 package com.joj.app;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+    private EditText fillUser;
+    private EditText fillPass;
+    private EditText fillPassAgain;
+    private CheckBox agree;
+    private Button signup;
+    private Intent intent;
+    private AlertDialog.Builder dialog;
+    private boolean checkbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initComponent();
+        actionListener();
     }
+
+    public void initComponent() {
+        this.fillUser = (EditText) findViewById(R.id.textFillUser);
+        this.fillPass = (EditText) findViewById(R.id.textFillPass);
+        this.fillPassAgain = (EditText) findViewById(R.id.textFillPassAgain);
+        this.agree = (CheckBox) findViewById(R.id.checkBox);
+        this.signup = (Button) findViewById(R.id.btnSignup);
+        this.dialog = new AlertDialog.Builder(this);
+    }
+
+    public void actionListener() {
+        this.agree.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        checkbox = true;
+                    }
+                }
+        );
+        this.signup.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (isAgree()) {
+                            intent = new Intent(SignUp.this, Store.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            showDialog();
+                        }
+                    }
+                }
+        );
+
+    }
+
+    public boolean isAgree() {
+        if (checkbox) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void showDialog(){
+        dialog.setMessage("YOU MUST AGREE BEFORE SIGN UP");
+        dialog.show();
+    }
+
+
 }
